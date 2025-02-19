5b1a657f6629e60ba43146c2f9fbb65072118f9f
HIRUN <hirundesilva97@gmail.com>
2025-02-18 21:01:39 -0500

    Task 1 - Created Employee Class

--- cc_9.js
+++ cc_9.js
@@ -1,1 +1,23 @@
-// Task 1
+// Task 1 - Created Employee Class
+class Employee {    //CREATE CLASS Employee WITH PROPERTIES NAME, ID, DEPARTMENT, AND SALARY
+    constructor(name, id, department, salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+
+getDetails() {      //ADD METHOD getDetails() THAT RETURNS FORMATTED STRING OF EMPLOYEE DETAILS
+    console.log(`Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`);
+}
+
+calculateAnnualSalary() {       //ADD METHOD calculateAnnualSalary THAT RETURNS EMPLOYEE'S ANNNUAL SALARY
+    console.log(`${this.salary * 12}`) 
+}
+}
+console.log ("---Task 1---")    //INSTANTIATE Employee as emp1, LOGGED "Task 1", AND emp1.getDetails();, AND emp1.calculateAnnualSalary(); 
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+emp1.getDetails();  
+// Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+emp1.calculateAnnualSalary(); 
+// Expected output: 60000
